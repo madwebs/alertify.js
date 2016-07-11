7d9f615ba799c5fffccaca688ecf04d4b31d7930
Martino Lessio <mlessio@users.noreply.github.com>
2016-07-11 17:27:27 +0200

    Update alertify.js.d.ts
    angular2 support

--- alertify.js.d.ts
+++ alertify.js.d.ts
@@ -3,30 +3,31 @@
 // Definitions by: Vlad Jerca <https://github.com/vladjerca>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-interface IAlertify {
-    reset(): IAlertify;
-    alert(message: string, onOkay?: Function, onCancel?: Function): IAlertify;
-    confirm(message: string, onOkay?: Function, onCancel?: Function): IAlertify;
-    prompt(message: string, onOkay?: Function, onCancel?: Function): IAlertify;
-    log(message: string, click?: Function): IAlertify;
-    theme(themeName: string): IAlertify;
-    success(message: string, click?: Function): IAlertify;
-    error(message: string, click?: Function): IAlertify;
-    cancelBtn(label: string): IAlertify;
-    okBtn(label: string): IAlertify;
-    delay(time: Number): IAlertify;
-    placeholder(str: string): IAlertify;
-    defaultValue(str: string): IAlertify;
-    maxLogItems(max: Number): IAlertify;
-    closeLogOnClick(bool: Boolean): IAlertify;
-    logPosition(position: string): IAlertify;
-    setLogTemplate(template: string): IAlertify;
-    clearLogs(): IAlertify;
-    parent(prt: HTMLElement): IAlertify;
+
+declare namespace alertify {
+    interface IAlertify {
+        reset(): IAlertify;
+        alert(message: string, onOkay?: Function, onCancel?: Function): IAlertify;
+        confirm(message: string, onOkay?: Function, onCancel?: Function): IAlertify;
+        prompt(message: string, onOkay?: Function, onCancel?: Function): IAlertify;
+        log(message: string, click?: Function): IAlertify;
+        theme(themeName: string): IAlertify;
+        success(message: string, click?: Function): IAlertify;
+        error(message: string, click?: Function): IAlertify;
+        cancelBtn(label: string): IAlertify;
+        okBtn(label: string): IAlertify;
+        delay(time: Number): IAlertify;
+        placeholder(str: string): IAlertify;
+        defaultValue(str: string): IAlertify;
+        maxLogItems(max: Number): IAlertify;
+        closeLogOnClick(bool: Boolean): IAlertify;
+        logPosition(position: string): IAlertify;
+        setLogTemplate(template: string): IAlertify;
+        clearLogs(): IAlertify;
+        parent(prt: HTMLElement): IAlertify;
+    }
+
 }
 
-declare var alertify: IAlertify;
+declare var alertify: alertify.IAlertify;
 
-declare module "alertify" {
-    export default alertify;
-}
